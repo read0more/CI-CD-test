52e2b8cf57040085bb0dd52996718dc88e00f547
ykpark <read0more@gmail.com>
2022-03-31 14:19:53 +0900

    Jest 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
   extends: ["airbnb-base", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  plugins: ["jest"],
 };
